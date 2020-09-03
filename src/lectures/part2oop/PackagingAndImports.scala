9ca8665f090b38812b36c5deb7d656d101e9faf1
lolpirate <debamalyadawn21@gmail.com>
2020-09-03 19:56:09 +0530

    Completed Lecture 17 (Packaging and Imports)

---
+++ src/lectures/part2oop/PackagingAndImports.scala
@@ -0,0 +1,7 @@
+package lectures.part2oop
+
+object PackagingAndImports extends App {
+  val list = exercises.part2oop.InheritanceAndTraits.Cons(1, exercises.part2oop.InheritanceAndTraits.Empty)
+  // fully qualified name
+  print(list)
+}

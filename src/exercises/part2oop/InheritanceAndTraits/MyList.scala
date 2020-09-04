29af6f7be79eeefb8078dac7926f37b73138bf04
lolpirate <debamalyadawn21@gmail.com>
2020-09-04 15:33:21 +0530

    Updated Code

--- src/exercises/part2oop/InheritanceAndTraits/MyList.scala
+++ src/exercises/part2oop/InheritanceAndTraits/MyList.scala
@@ -29,6 +29,14 @@ abstract class MyList[+A] {
   def filter(predicate: A => Boolean): MyList[A]
 
   def ++[B >: A](list: MyList[B]): MyList[B]
+
+  def foreach(func: A => Unit): Unit
+
+  def sort(compare: (A, A) => Int): MyList[A]
+
+  def zipWith[B, C](list: MyList[B], func: (A, B) => C): MyList[C]
+
+  def fold[B](start: B)(func: (B, A) => B): B
 }
 
 case object Empty extends MyList[Nothing] {
@@ -49,6 +57,17 @@ case object Empty extends MyList[Nothing] {
   def filter(predicate: Nothing => Boolean): MyList[Nothing] = Empty
 
   override def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
+
+  override def foreach(func: Nothing => Unit): Unit = Empty
+
+  override def sort(func: (Nothing, Nothing) => Int): MyList[Nothing] = Empty
+
+  override def zipWith[B, C](list: MyList[B], func: (Nothing, B) => C): MyList[C] = {
+    if (!list.isEmpty) throw new RuntimeException("List lengths do not match")
+    else Empty
+  }
+
+  override def fold[B](start: B)(func: (B, Nothing) => B): B = start
 }
 
 case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
@@ -80,24 +99,54 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
   override def flatmap[B](transformer: A => MyList[B]): MyList[B] = {
     transformer(h) ++ t.flatmap(transformer)
   }
+
+  override def foreach(func: A => Unit): Unit = {
+    if (t.isEmpty) func(h)
+    else {
+      func(h)
+      t.foreach(func)
+    }
+  }
+
+  override def sort(compare: (A, A) => Int): MyList[A] = {
+    def insert(x: A, sortedList: MyList[A]): MyList[A] = {
+      if (sortedList.isEmpty) new Cons(x, Empty)
+      else if (compare(x, sortedList.head) < 0) new Cons[A](x, sortedList)
+      else new Cons[A](sortedList.head, insert(x, sortedList.tail))
+    }
+
+    val sortedTail = t.sort(compare)
+    insert(h, sortedTail)
+  }
+
+  override def zipWith[B, C](list: MyList[B], func: (A, B) => C): MyList[C] = {
+    if (list.isEmpty) throw new RuntimeException("List lengths do not match")
+    else new Cons(func(head, list.head), t.zipWith(list.tail, func))
+  }
+
+  override def fold[B](start: B)(func: (B, A) => B): B = {
+    val newStart = func(start, h)
+    t.fold(newStart)(func)
+  }
 }
 
 object ListTest extends App {
   var list: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
   var list2: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
-  println(list.head)
+  var list3 = Empty
+  /*println(list.head)
   println(list.tail.head)
   list = list.add(4)
   println(list)
-  println(list.map(new Function1[Int, Int] {
-    override def apply(v1: Int): Int = v1 * 2
-  }))
-  println(list.filter(new Function1[Int, Boolean] {
-    override def apply(v1: Int): Boolean = v1 % 2 == 0
-  }))
+  println(list.map((x: Int) => x * 2))
+  println(list.filter((x: Int) => x % 2 == 0))
   println(list ++ list2)
-  println(list2.flatmap(new Function1[Int, MyList[Int]] {
-    override def apply(v1: Int): MyList[Int] = new Cons(v1, new Cons(v1 + 1, Empty))
-  }))
-  println(list2.map(x => x * 3))
+  println(list2.flatmap((x: Int) => new Cons[Int](x, new Cons[Int](x+1, Empty))))
+  println(list2.map(x => x * 3))*/
+  list2.foreach((x: Int) => println(x))
+  println(list.zipWith(list2, (x: Int, y: Int) => x * y))
+  //println(list3.zipWith(list, (x: Int, y: Int) => x * y))
+  println(list.sort(_ - _))
+  println(list.fold(0)(_ + _))
+
 }
